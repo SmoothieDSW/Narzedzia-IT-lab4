0d7e54997d23e603fb5b37d91cddab99663084b1
SmoothieDSW <jakubgadcek003@gmail.com>
2026-06-04 20:23:24 +0200

    Dodano tworzenie plikow log z obsluga argumentu liczby

--- narzedzie.sh
+++ narzedzie.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 if [ "$1" == "--date" ]; then
     date
+elif [ "$1" == "--logs" ]; then
+    LICZBA=${2:-100}
+    for ((i=1; i<=LICZBA; i++)); do
+        NAZWA_PLIKU="log${i}.txt"
+        echo "Nazwa pliku: $NAZWA_PLIKU" > $NAZWA_PLIKU
+        echo "Skrypt: $0" >> $NAZWA_PLIKU
+        echo "Data: $(date)" >> $NAZWA_PLIKU
+    done
 fi
