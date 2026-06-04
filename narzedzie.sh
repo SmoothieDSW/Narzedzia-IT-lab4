597346cc2a73c7d7149d07c1a2530fd6f74d669e
SmoothieDSW <jakubgadcek003@gmail.com>
2026-06-04 20:27:24 +0200

    Dodano krotkie warianty flag

--- narzedzie.sh
+++ narzedzie.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-if [ "$1" == "--date" ]; then
-    date
-elif [ "$1" == "--logs" ]; then
-    LICZBA=${2:-100}
-    for ((i=1; i<=LICZBA; i++)); do
-        NAZWA_PLIKU="log${i}.txt"
-        echo "Nazwa pliku: $NAZWA_PLIKU" > $NAZWA_PLIKU
-        echo "Skrypt: $0" >> $NAZWA_PLIKU
-        echo "Data: $(date)" >> $NAZWA_PLIKU
-    done
-elif [ "$1" == "--help" ]; then
-    echo "Dostepne opcje:"
-    echo "  --date      - Wyswietla dzisiejsza date"
-    echo "  --logs [N]  - Tworzy N plikow log (domyslnie 100)"
-    echo "  --help      - Wyswietla te pomoc"
-fi
+case "$1" in
+    --date|-d)
+        date
+        ;;
+    --logs|-l)
+        LICZBA=${2:-100}
+        for ((i=1; i<=LICZBA; i++)); do
+            NAZWA_PLIKU="log${i}.txt"
+            echo "Nazwa pliku: $NAZWA_PLIKU" > $NAZWA_PLIKU
+            echo "Skrypt: $0" >> $NAZWA_PLIKU
+            echo "Data: $(date)" >> $NAZWA_PLIKU
+        done
+        ;;
+    --help|-h)
+        echo "Dostepne opcje:"
+        echo "  --date, -d      - Wyswietla dzisiejsza date"
+        echo "  --logs, -l [N]  - Tworzy N plikow log (domyslnie 100)"
+        echo "  --help, -h      - Wyswietla te pomoc"
+        ;;
+esac
