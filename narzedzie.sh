6b5a26dd88c28d4faa7b8bdfdb6e444bede1adc9
SmoothieDSW <jakubgadcek003@gmail.com>
2026-06-04 20:29:29 +0200

    Dodano obsluge flagi init oraz zmiennej PATH

--- narzedzie.sh
+++ narzedzie.sh
@@ -17,5 +17,10 @@ case "$1" in
         echo "  --date, -d      - Wyswietla dzisiejsza date"
         echo "  --logs, -l [N]  - Tworzy N plikow log (domyslnie 100)"
         echo "  --help, -h      - Wyswietla te pomoc"
+    --init)
+        URL_REPOZYTORIUM=$(git config --get remote.origin.url)
+        git clone "$URL_REPOZYTORIUM" .
+        export PATH="$PATH:$(pwd)"
+        echo "Repozytorium sklonowane, sciezka PATH zaktualizowana."
         ;;
 esac
